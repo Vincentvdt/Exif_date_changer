0e002292d9819545ef5e53eea26c0d1e1dff1094
Vincentvdt <vincent.vidot3@gmail.com>
2023-01-28 19:57:19 +0100

    Separate all the process of .jpg into function process_jpg.image() Change date_formats into a global variable rename some variables

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ elif overwrite == "no":
 
 rename = pyip.inputYesNo(prompt="Dou you want to rename the file if a date is found in the exif data (only for .jpg "
                                 "and .jpeg) [Y/n]")
-
 if rename == "yes":
     rename = True
 elif rename == "no":
@@ -45,14 +44,17 @@ regex_pattern = [
 
 def generate_date_time_formats():
     separators = ['-', '.', '_', '']
-    date_formats = ["%Y{}%m{}%d".format(sep, sep) for sep in separators]
-    date_formats.append("%Y%m%d")
+    _date_formats = ["%Y{}%m{}%d".format(sep, sep) for sep in separators]
+    _date_formats.append("%Y%m%d")
     time_formats = ["%H{}%M{}%S".format(sep, sep) for sep in separators]
     time_formats.append("%H%M%S")
-    date_time_formats = [date_format + separator + time_format for date_format in date_formats for separator in
+    date_time_formats = [date_format + separator + time_format for date_format in _date_formats for separator in
                          separators for time_format in time_formats]
-    date_time_formats += date_formats
+    date_time_formats += _date_formats
     return date_time_formats
+
+
+date_formats = generate_date_time_formats()
 
 
 def print_final_count(counter, _destination_folder):
@@ -74,14 +76,15 @@ def print_error_message(msg):
 
 def copy_image_to_folder(source, destination_name, no_date_found=False):
     try:
-        destination = os.path.join(destination_folder, destination_name)
-        os.makedirs(os.path.dirname(destination), exist_ok=True)
-        if os.path.exists(destination) and not overwrite:
-            raise FileExistsError(f"'{os.path.basename(destination)}' "
-                                  f"already exists at '{os.path.relpath(destination)}'.\n "
+        img_destination_path = os.path.join(destination_folder, destination_name)
+        os.makedirs(os.path.dirname(img_destination_path), exist_ok=True)
+        if os.path.exists(img_destination_path) and not overwrite:
+            raise FileExistsError(f"'{os.path.basename(img_destination_path)}' "
+                                  f"already exists at '{os.path.relpath(img_destination_path)}'.\n "
                                   f"To overwrite the file, please enable the 'overwrite' option in the user interface.")
-        shutil.copy2(source, destination)
-        print(f"The file '{os.path.basename(source)}' has been copied to the directory '{os.path.dirname(destination)}'"
+        shutil.copy2(source, img_destination_path)
+        print(f"The file '{os.path.basename(source)}' has been copied to "
+              f"the directory '{os.path.dirname(img_destination_path)}'"
               f"{' BUT no date was found in the image name.' if no_date_found else '.'}")
 
         return True
@@ -100,26 +103,7 @@ def copy_image_to_folder(source, destination_name, no_date_found=False):
         return False
 
 
-def process_exif_image(img):
-    if rename:
-        name = f'{img["date"].strftime(name_date_format)}_{img["name"]}{img["ext"]}'
-    else:
-        name = img["file"]
-    is_success = copy_image_to_folder(img["file"], name)
-    destination = os.path.join(destination_folder, name)
-    change_created_date(destination, img["date"])
-    return is_success
-
-
-def process_png_image(file, destination, date):
-    if not date:
-        update_counter(copy_image_to_folder(file, destination, True))
-    elif date:
-        update_counter(copy_image_to_folder(file, destination))
-        change_created_date(destination, date)
-
-
-def change_created_date(img_path, date):
+def change_created_date(date, img_path):
     img = filedate.File(img_path)
     img.set(
         created=date.strftime("%Y-%m-%d %H:%M:%S"),
@@ -132,7 +116,7 @@ def update_counter(is_success: bool):
     count['error'] += (1 - is_success)
 
 
-def find_date_in_name(file, date_formats, _regex_pattern: list = None):
+def find_date_in_name(file, _regex_pattern: list = None):
     file_name, file_extension = os.path.splitext(file)
     if _regex_pattern is None:
         _regex_pattern = [
@@ -167,58 +151,81 @@ def check_extension(file):
         print(e)
 
 
-def process_no_exif_image(file, dst_folder, date_formats):
+def process_no_exif_image(file, dst_folder):
     no_exif_folder = os.path.join(dst_folder, "NoExif")
     os.makedirs(no_exif_folder, exist_ok=True)
-    date = find_date_in_name(file, date_formats)
+    date = find_date_in_name(file)
     if date:
-        destination = os.path.join(no_exif_folder, file)
-        is_success = copy_image_to_folder(file, destination)
-        change_created_date(destination, date)
+        img_destination_path = os.path.join(no_exif_folder, file)
+        is_success = copy_image_to_folder(file, img_destination_path)
+        change_created_date(date, img_destination_path)
     else:
         no_exif_and_date_folder = os.path.join(no_exif_folder, "NoDate")
         os.makedirs(no_exif_and_date_folder, exist_ok=True)
-        destination = os.path.join(no_exif_and_date_folder, file)
-        is_success = copy_image_to_folder(file, destination)
+        img_destination_path = os.path.join(no_exif_and_date_folder, file)
+        is_success = copy_image_to_folder(file, img_destination_path)
     return is_success
 
 
-def exif_date_change(src_folder, dst_folder):
-    date_formats = generate_date_time_formats()
-    os.makedirs(dst_folder, exist_ok=True)
-    files = [file for file in os.listdir(src_folder) if os.path.isfile(file) and '.' in os.path.basename(file)]
-    file_count = len(files)
-    print(file_count)
-    for file in files:
-        file_name, file_extension = os.path.splitext(file)
-        check_extension(file)
-        if file_extension.lower() in (".jpg", ".jpeg"):
-            with open(file, 'rb'):
-                img = Image(file)
-                if not img.has_exif:
-                    update_counter(process_no_exif_image(file, dst_folder, date_formats))
-                else:
-                    date, time = img.datetime.split(' ')
-                    img_exif_date = datetime.strptime(date + ' ' + time, "%Y:%m:%d %H:%M:%S")
-                    img = {
-                        "file": file,
-                        "name": file_name,
-                        "ext": file_extension,
-                        "date": img_exif_date
-                    }
-                    update_counter(process_exif_image(img))
+def process_exif_image(img, dst_folder):
+    image, name, ext, date = img.values()
+    if rename:
+        name = f'{date.strftime(name_date_format)}_{name}{ext}'
+    else:
+        name = image
 
-        elif file_extension.lower() == '.png':
-            png_folder = os.path.join(dst_folder, "PngFiles")
-            os.makedirs(png_folder, exist_ok=True)
+    is_success = copy_image_to_folder(img["file"], name)
+    img_destination_path = os.path.join(dst_folder, name)
+    change_created_date(img["date"], img_destination_path)
 
-            date = find_date_in_name(file, date_formats)
+    return is_success
 
-            destination = os.path.join(png_folder, file)
-            process_png_image(file, destination, date)
+
+def process_jpg_image(image, dst_folder):
+    with open(image, 'rb'):
+        img = Image(image)
+        image_name, image_extension = os.path.splitext(image)
+        if not img.has_exif:
+            update_counter(process_no_exif_image(image, dst_folder))
         else:
+            date, time = img.datetime.split(' ')
+            img_exif_date = datetime.strptime(date + ' ' + time, "%Y:%m:%d %H:%M:%S")
+            img = {
+                "image": image,
+                "name": image_name,
+                "ext": image_extension,
+                "date": img_exif_date
+            }
+            update_counter(process_exif_image(img, dst_folder))
+
+
+def process_png_image(image, dst_folder):
+    png_folder = os.path.join(dst_folder, "PngFiles")
+    os.makedirs(png_folder, exist_ok=True)
+    date = find_date_in_name(image, date_formats)
+    img_destination_path = os.path.join(png_folder, image)
+    if not date:
+        update_counter(copy_image_to_folder(image, img_destination_path, True))
+    elif date:
+        update_counter(copy_image_to_folder(image, img_destination_path))
+        change_created_date(date, img_destination_path)
+
+
+def exif_date_change(src_folder, dst_folder):
+    os.makedirs(dst_folder, exist_ok=True)
+    for file in os.listdir(src_folder):
+        file_name, file_extension = os.path.splitext(file)
+
+        if not os.path.isfile(file):
             continue
 
+        check_extension(file)
+        if file_extension.lower() in (".jpg", ".jpeg"):
+            process_jpg_image(file, dst_folder)
+        elif file_extension.lower() == '.png':
+            process_png_image(file, dst_folder)
+        else:
+            continue
     print_final_count(count, destination_folder)
 
 
