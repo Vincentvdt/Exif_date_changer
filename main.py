18d6d1f0cf8ccf2b1a00454f359a9c15ec59433c
vincentvdt <vincent.vidot3@gmail.com>
2024-01-02 23:39:36 +0100

    refactoring

--- main.py
+++ main.py
@@ -21,42 +21,46 @@ def get_all_pictures(folder_path):
 
 
 def get_exif_date(picture):
-    exif_data = picture.getexif()
-    date = exif_data.get(306)
-    if date and date != '0000:00:00 00:00:00':
-        return datetime.strptime(date, "%Y:%m:%d %H:%M:%S")
-    else:
+    try:
+        exif_data = picture.getexif()
+        date = exif_data.get(306)
+        if date and date != '0000:00:00 00:00:00':
+            return datetime.strptime(date, "%Y:%m:%d %H:%M:%S")
+        else:
+            return None
+    except Exception as exif_error:
+        logging.error(f"[{picture.name}] Error extracting EXIF data: {exif_error}")
         return None
 
 
 def change_created_date(img_path, date):
     if date:
-        date = date.strftime("%Y-%m-%d %H:%M:%S")
-        img = filedate.File(img_path)
-        img.set(
-            created=date,
-            modified=date
-        )
+        date_str = date.strftime("%Y-%m-%d %H:%M:%S")
+        try:
+            img = filedate.File(img_path)
+            img.set(created=date_str, modified=date_str)
+        except Exception as filedate_error:
+            logging.error(f"[{img_path.name}] Error setting file dates: {filedate_error}")
 
 
 def create_copy(source_file):
     relative_path = source_file.relative_to(SOURCE_FOLDER_PATH)
     dest_file = Path(DEST_FOLDER_PATH, relative_path)
     dest_file.parent.mkdir(parents=True, exist_ok=True)
-    shutil.copy2(source_file, dest_file)
-    return dest_file
+    try:
+        shutil.copy2(source_file, dest_file)
+        return dest_file
+    except Exception as copy_error:
+        logging.error(f"[{source_file.name}] Error copying file: {copy_error}")
+        return None
 
 
 def generate_date_time_formats():
     separators = ['-', '.', '_', '']
-    _date_formats = [f"%Y{sep}%m{sep}%d" for sep in separators]
-    _date_formats += [f"%d{sep}%m{sep}%Y" for sep in separators]
-
-    time_formats = [f"%H{sep}%M{sep}%S" for sep in separators]
-    date_time_formats = [f"{_date_format}{separator}{time_format}" for _date_format in _date_formats for separator in
-                         separators for time_format in time_formats]
-    date_time_formats += _date_formats
-    return date_time_formats
+    dates = [f"%Y{sep}%m{sep}%d" for sep in separators] + [f"%d{sep}%m{sep}%Y" for sep in separators]
+    times = [f"%H{sep}%M{sep}%S" for sep in separators]
+    return [f"{_date_format}{separator}{time_format}" for _date_format in dates
+            for separator in separators for time_format in times] + dates
 
 
 date_formats = generate_date_time_formats()
@@ -64,7 +68,7 @@ date_formats = generate_date_time_formats()
 
 def find_date_in_name(picture):
     name = picture.stem
-    regex_pattern = [
+    regex_patterns = [
         r"(\d{4}[-._]\d{2}[-._]\d{2}[-._]\d{2}[-._]\d{2}[-._]\d{2})",
         r"(\d{2}[-._]\d{2}[-._]\d{4}[-._]\d{2}[-._]\d{2}[-._]\d{2})",
         r"(\d{4}[-._]\d{2}[-._]\d{2}[-._]\d{6})",
@@ -78,7 +82,7 @@ def find_date_in_name(picture):
     ]
     current_year = datetime.today().year
 
-    for pattern in regex_pattern:
+    for pattern in regex_patterns:
         match = re.search(pattern, name)
         if match:
             matched_string = match.group(1)
@@ -89,25 +93,28 @@ def find_date_in_name(picture):
                         return parsed_date
                 except ValueError:
                     continue
-        if not match:
-            continue
+    return None
 
 
 def main():
     pictures = get_all_pictures(SOURCE_FOLDER_PATH)
-    pbar = tqdm(pictures, leave=True, smoothing=1)
-    for picture in pbar:
-        pbar.set_description("Progress", refresh=True)
-        pbar.set_postfix(file="{}".format(picture))
-        try:
-            with Image.open(picture) as image:
-                date = get_exif_date(image)
-                new_picture = create_copy(picture)
-                if date is None:
-                    date = find_date_in_name(new_picture)
-                change_created_date(new_picture, date)
-        except Exception as error:
-            logging.error(f"[{picture.name}] An exception occurred: {error}")
+    with tqdm(pictures, leave=True, smoothing=1) as pbar:
+        for picture in pbar:
+            pbar.set_description("Progress", refresh=True)
+            pbar.set_postfix(file="{}".format(picture))
+            try:
+                with Image.open(picture) as image:
+                    date = get_exif_date(image) or find_date_in_name(picture)
+                    if date is None:
+                        logging.warning(f"[{picture.name}] Unable to determine creation date.")
+                    new_picture = create_copy(picture)
+                    if new_picture:
+                        change_created_date(new_picture, date)
+                    else:
+                        logging.error(f"[{picture.name}] Skipping due to copy error.")
+            except Exception as error:
+                logging.error(f"[{picture.name}] An exception occurred: {error}")
+                print(f"Error processing {picture.name}: {error}")
 
 
 if __name__ == "__main__":
